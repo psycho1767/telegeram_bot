5198a177912e0a2f9fd02f453c3a892a2b30b159
psycho <77152990+psycho1767@users.noreply.github.com>
2024-06-04 14:26:30 +0000

    ,fh

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 import random
 import string
+import sqlite3
 
+conn =  sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+
+def fetch_codes():
+    cursor.execute('SELECT * FROM codes')
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        print(row)
+        
+fetch_codes()
+            
 def file_code(length=5):
     characters = string.ascii_lowercase + string.digits
     code = ''.join(random.choices(characters, k=length))
@@ -13,10 +27,18 @@ async def handle_all_messages(update: Update, context: ContextTypes.DEFAULT_TYPE
         await update.message.reply_text(f'شما یک پیام متنی ارسال کردید: {update.message.text}')
     elif update.message.video:
         file_id = update.message.video.file_id
-        await update.message.reply_video(video=file_id, caption=f'کد فایل : {file_code()}')
+        file_code = file_code()
+        await cursor.execute(f'INSERT INTO codes (code,file_id,type) VALUES ({file_code},{file_id},"video")')
+        await update.message.reply_text(f'فایل با موفقیت ثبت شد کد فایل: \n `{file_code}`')
+
+        fetch_codes()
     elif update.message.document:
         file_id = update.message.document.file_id
-        await update.message.reply_document(document=file_id, caption=f'کد فایل : {file_code()}')
+        file_code = file_code()
+        await cursor.execute(f'INSERT INTO codes (code,file_id,type) VALUES ({file_code},{file_id},"document")')
+        await update.message.reply_text(f'فایل با موفقیت ثبت شد کد فایل: \n `{file_code}`')
+        
+        fetch_codes()
     else:
         print(update.message)
 
